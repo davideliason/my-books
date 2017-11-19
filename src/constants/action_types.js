48e013e0623ded44f52975d437042d84c2cee1f6
davideliason <davethemaker@gmail.com>
2017-11-19 09:45:45 -0800

    add actionTypes related to querying the FB DB

---
+++ src/constants/action_types.js
@@ -0,0 +1,13 @@
+const actionTypes = {
+  GetBooksRequested: 'GET_BOOKS_REQUESTED',
+  GetBooksRejected: 'GET_BOOKS_REJECTED',
+  GetBooksFulfilled: 'GET_BOOKS_FULFILLED',
+
+  AddToBooksRequested: 'ADD_TO_BOOKS_REQUESTED',
+  AddToBooksRejected: 'ADD_TO_BOOKS_REJECTED',
+  AddToBooksFulfilled: 'ADD_TO_BOOKS_FULFILLED',
+
+  BookAdded: 'BOOK_ADDED'
+};
+
+export default actionTypes;
