29b7049f6a98240ed1b139beb79c3ac9224542b3
davideliason <davethemaker@gmail.com>
2017-11-19 11:14:54 -0800

    create second reducer for state filter

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
-import book_reducer from '../reducers/books.js';
+import books from '../reducers/books.js';
 
 const root_reducer = combineReducers({
-	books: book_reducer
+	books
 });
 
 export default root_reducer;
