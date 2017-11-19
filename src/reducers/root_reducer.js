f8bbdcf58e45f626ee2383c41266c80a1598243b
davideliason <davethemaker@gmail.com>
2017-11-19 10:13:22 -0800

    simplify the store with starting with just the book_reducer

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,8 +1,8 @@
 import {combineReducers} from 'redux';
 import book_reducer from '../reducers/book_reducer.js';
 
-const store = combineReducers({
+const root_reducer = combineReducers({
 	books: book_reducer
 });
 
-export default store;
+export default root_reducer;
