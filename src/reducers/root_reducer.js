25d5702f350cc838b087facf1fa7fcfc92ef8764
davideliason <davethemaker@gmail.com>
2017-11-19 11:17:41 -0800

    combinereducer successfully creates state level object with books array and genrefilter string

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import books from '../reducers/books.js';
+import genreFilter from '../reducers/genreFilter.js';
 
 const root_reducer = combineReducers({
-	books
+	books,
+	genreFilter
 });
 
 export default root_reducer;
