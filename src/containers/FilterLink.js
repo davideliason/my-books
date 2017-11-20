830ae6f573e7e8da0ad154f3dca6033cc4d1ac04
davideliason <davethemaker@gmail.com>
2017-11-20 14:36:01 -0800

    create connect container for Link passing in setGenreFilter

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -2,22 +2,13 @@ import { connect } from 'react-redux'
 import { setGenreFilter } from '../actions'
 import LinkFilter from '../components/LinkFilter'
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    active: ownProps.filter === state.visibilityFilter
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: () => {
+    dispatch(setGenreFilter(ownProps.filter))
   }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onClick: () => {
-      dispatch(setVisibilityFilter(ownProps.filter))
-    }
-  }
-}
+})
 
 const FilterLink = connect(
-  mapStateToProps,
   mapDispatchToProps
 )(Link)
 
