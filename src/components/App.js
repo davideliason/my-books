5d25a5bb26a19697c469b703bcbbfd473549852d
davideliason <davethemaker@gmail.com>
2017-11-22 15:29:41 -0800

    add grid for styling of buttons and book display info

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,27 @@ import FilteredBookList from '../containers/FilteredBookList';
 import UpdateMysteryGenre from '../containers/UpdateMysteryGenre.js';
 import UpdateSciFiGenre from '../containers/UpdateSciFiGenre.js';
 import UpdateShowAllGenre from '../containers/UpdateShowAllGenre.js';
+import {ButtonToolbar, ButtonGroup,Grid, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
+      	<Grid>
+      	  <Row>
+      	  	<Col xs={4}>
+      	 	  <UpdateMysteryGenre />
+      	 	</Col>
+      	 	<Col xs={4}>
+      	 	  <UpdateSciFiGenre />
+      	 	</Col><Col xs={4}>
+      	 	  <UpdateShowAllGenre />
+      	 	</Col>
+      	  </Row>
+      	 </Grid>
      	 <FilteredBookList />
-     	 <UpdateMysteryGenre />
-     	 <UpdateSciFiGenre />
-     	 <UpdateShowAllGenre />
+
       </div>
     );
   }
