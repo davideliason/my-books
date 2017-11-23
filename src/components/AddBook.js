fd6ad4b889768b21844f20e557e3c57998280327
davideliason <davethemaker@gmail.com>
2017-11-23 10:03:18 -0800

    successfully create input form using refs to identify needed input text fields, capturing those data points and populating the arguments for the passed dispatch updating state with new book object

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -4,18 +4,24 @@ import React, { Component, PropTypes } from 'react'
 class AddBook extends Component {
 
     handleSubmit(event) {
-        let input = this.refs.input;
+        let inputTitle = this.refs.title;
+        let inputAuthor = this.refs.author;
+        let inputGenre = this.refs.genre;
+
         event.preventDefault();
        
-        this.props.addBook(input.value,"jim","mystery",false);
-        input.value = ''
+        this.props.addBook(inputTitle.value,inputAuthor.value,inputGenre.value,false);
+        inputTitle.value = ''
     }
 
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit.bind(this)}>
-                    <input ref="input"/>
+                    <input placeholder="title" ref="title"/>
+                    <input placeholder="author"ref="author"/>
+                    <input placeholder="genre"ref="genre"/>
+
                     <button type="submit">
                         Add Book
                     </button>
