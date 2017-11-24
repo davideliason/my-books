7cf139243353fdd65fb6799c3c8236c4895dbfcc
davideliason <davethemaker@gmail.com>
2017-11-24 06:14:23 -0800

    add redux-thunk to middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import root_reducer from '../reducers/root_reducer.js';
-import {createLogger} from "redux-logger";
 
 const logger = createLogger();
 
 const store = createStore(root_reducer,
   {},
-  applyMiddleware(logger)
+  applyMiddleware(thunk,logger)
 );
 
 export default store;
